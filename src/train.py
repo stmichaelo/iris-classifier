1da9f29c09e62b263ef98838cb500f57afdc65ba
stmichaelo <stmichaelo@yahoo.com>
2025-12-23 13:13:32 +0000

    Implement confusion matrix and save model
    Added confusion matrix visualization and model saving.

--- src/train.py
+++ src/train.py
@@ -32,3 +32,13 @@ model2 = KNeighborsClassifier(n_neighbors=5)
 model2.fit(X_train, y_train)
 y_pred2 = model2.predict(X_test)
 print("k-NN accuracy:", accuracy_score(y_test, y_pred2))
+
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+y_true = [1,0,2,1,1]
+y_pred = [1,0,2,1,1]
+cm = confusion_matrix(y_true, y_pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+disp.plot().figure_.savefig('confusion_matrix.png')
+
+import joblib
+joblib.dump(model, "model.joblib")
